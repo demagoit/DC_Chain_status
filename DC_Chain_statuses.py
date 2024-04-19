c950a59493faaea788c5e5b356537a41455a3b3c
demagoit <33demagoit@gmail.com>
2024-04-19 18:22:07 +0300

    autoreplacement_evaluation added

--- DC_Chain_statuses.py
+++ DC_Chain_statuses.py
@@ -14,7 +14,11 @@ in_file = {
     'stat_cross_ref': 'Stat_cross_ref'
 }
 
-out_file = {'name': 'Status_verification.xlsx'}
+out_file = {
+    'name': 'Status_verification.xlsx',
+    'status_sheet': 'Statuses', 
+    'autoreplacement': 'Autoreplacement'
+    }
             
 print(f'From {in_file["name"]} take:\n',
       f'statuses - from {in_file["status_sheet"]}\n',
@@ -119,66 +123,88 @@ def df_preparation_pipeline():
         lambda x: x.dropna().index.to_list())
     df_stats_ref = df_stats_ref[['B600_accepted']]
 
+def df_merge_pipeline():
+    global df_statuses
+    global df_products
+    global df_pl_plant
+    global df_stats_ref
+    global df_output
+
+    df_statuses = df_statuses.join(
+        df_products, on='Material', how='outer').reset_index(drop=True)
+    df_statuses[['X-distr.chain_status', 'Plant-Sp.Matl_Status',  'DChain_Status']
+                ] = df_statuses[['X-distr.chain_status', 'Plant-Sp.Matl_Status', 'DChain_Status']].replace(np.nan, 0)
+    df_statuses = df_statuses.astype(
+        {'X-distr.chain_status': 'int64', 'Plant-Sp.Matl_Status': 'int64', 'DChain_Status': 'int64'})
+
+    df_statuses = df_statuses.join(
+        df_stats_ref, on='X-distr.chain_status', how='left')
+    df_statuses['B600_accepted'] = df_statuses['B600_accepted'].apply(
+        lambda x: x if isinstance(x, list) else [])
+
+    df_statuses = df_statuses.join(df_pl_plant, on='Product_line', how='left')
+    df_statuses['Plant_accepted'] = df_statuses['Plant_accepted'].apply(
+        lambda x: x if isinstance(x, list) else [])
+
+    # local statuses are the same? (recomended to be the same)
+    df_statuses['Is_local_stat_equal'] = df_statuses['Plant-Sp.Matl_Status'] == df_statuses['DChain_Status']
+
+    # DChain statuses are the same?
+    df_statuses['Is_X-DC_equal'] = df_statuses['X-distr.chain_status'] == df_statuses['DChain_Status']
+
+    # is current DC_status within accepted for B600 variants?
+    df_statuses['Is_Status_accepted'] = df_statuses[['DChain_Status', 'B600_accepted']].apply(
+        lambda row: row.iloc[0] in row.iloc[1], axis=1)
+
+    # is current DC_status 'blocked' if no price?
+    df_statuses['Is_Status_accepted'] = df_statuses[['Is_Status_accepted', 'DChain_Status', 'Amount_(Currency)']].apply(
+        lambda row: row.iloc[0] if row.iloc[2] else row.iloc[1] in [75, 78], axis=1)
+
+    # is current Plant within accepted for B600 variants?
+    df_statuses['Is_Plant_correct'] = df_statuses[[
+        'Plant', 'Plant_accepted']].apply(lambda row: row.iloc[0] in row.iloc[1], axis=1)
+
+    df_plant_ok = df_statuses[df_statuses['Is_Plant_correct'] == True]
+    df_plant_wrong = df_statuses.drop(df_plant_ok.index)
+
+    # from df_plant_wrong remove products that exist in df_plant_ok
+    df_plant_wrong = df_plant_wrong.drop(
+        df_plant_wrong[df_plant_wrong['Material'].isin(df_plant_ok['Material'].unique())].index)
+
+    # from df_plant_wrong remove products without price
+    df_plant_wrong = df_plant_wrong[~df_plant_wrong['Amount_(Currency)'].isna()]
+
+    df_output = pd.concat([df_plant_wrong, df_plant_ok])
+
+    df_output = df_output[df_output[['Amount_(Currency)', 'B600_accepted', 'Plant_accepted']].any(axis=1)]
+
+
+def autoreplacement_evaluation():
+    global df_auroreplacement
+    global df_output
+
+    df_statuses = df_output[['Material', 'Product_line', 'X-distr.chain_status', 'DChain_Status', 'Text_Material']]
+    df_statuses.set_index('Material', inplace=True)
+
+    df_auroreplacement = df_auroreplacement.join(
+        df_statuses, on='Material_Entered', how='left', rsuffix='_Old')
+
+    df_auroreplacement = df_auroreplacement.join(
+        df_statuses, on='Danf_Material_number', how='left', rsuffix='_New')
+    
+    # # order of steps matters as overwrite expected!!!
+    # df_auroreplacement['ToDo'] = None
+    # df_auroreplacement['ToDo'] = df_auroreplacement[['DChain_Status', 'DChain_Status_New', 'ToDo']].apply(
+    #     lambda x: 'OK' if x[1] == 51 else x[2])
+    # df_auroreplacement['ToDo'] = df_auroreplacement['DChain_Status_New'].apply(lambda x: 'review new code status' if x != 51 else x)
+    # df_auroreplacement['ToDo'] = df_auroreplacement['DChain_Status_New'].apply(lambda x: 'review replacement' if x in [57, 75, 78] else x)
+    # df_auroreplacement['ToDo'] = df_auroreplacement['DChain_Status'].apply(lambda x: 'cancel autoreplacement' if x in [57, 78] else x)
+    # # print(df_auroreplacement.head())
+    # # sys.exit(1)
+
 df_preparation_pipeline()
-
-# def df_merge_pipeline():
-df_statuses = df_statuses.join(
-    df_products, on='Material', how='outer').reset_index(drop=True)
-df_statuses[['X-distr.chain_status', 'Plant-Sp.Matl_Status',  'DChain_Status']
-            ] = df_statuses[['X-distr.chain_status', 'Plant-Sp.Matl_Status', 'DChain_Status']].replace(np.nan, 0)
-df_statuses = df_statuses.astype(
-    {'X-distr.chain_status': 'int64', 'Plant-Sp.Matl_Status': 'int64', 'DChain_Status': 'int64'})
-
-df_statuses = df_statuses.join(
-    df_stats_ref, on='X-distr.chain_status', how='left')
-df_statuses['B600_accepted'] = df_statuses['B600_accepted'].apply(
-    lambda x: x if isinstance(x, list) else [])
-
-df_statuses = df_statuses.join(df_pl_plant, on='Product_line', how='left')
-df_statuses['Plant_accepted'] = df_statuses['Plant_accepted'].apply(
-    lambda x: x if isinstance(x, list) else [])
-
-# print(df_statuses.head())
-# sys.exit(1)
-
-# local statuses are the same? (recomended to be the same)
-df_statuses['Is_local_stat_equal'] = df_statuses['Plant-Sp.Matl_Status'] == df_statuses['DChain_Status']
-
-# DChain statuses are the same?
-df_statuses['Is_X-DC_equal'] = df_statuses['X-distr.chain_status'] == df_statuses['DChain_Status']
-
-# is current DC_status within accepted for B600 variants?
-df_statuses['Is_Status_accepted'] = df_statuses[['DChain_Status', 'B600_accepted']].apply(
-    lambda row: row.iloc[0] in row.iloc[1], axis=1)
-
-# is current DC_status 'blocked' if no price?
-df_statuses['Is_Status_accepted'] = df_statuses[['Is_Status_accepted', 'DChain_Status', 'Amount_(Currency)']].apply(
-    lambda row: row.iloc[0] if row.iloc[2] else row.iloc[1] in [75, 78], axis=1)
-
-# is current Plant within accepted for B600 variants?
-df_statuses['Is_Plant_correct'] = df_statuses[[
-    'Plant', 'Plant_accepted']].apply(lambda row: row.iloc[0] in row.iloc[1], axis=1)
-
-# print(df_statuses)
-
-
-df_plant_ok = df_statuses[df_statuses['Is_Plant_correct'] == True]
-df_plant_wrong = df_statuses.drop(df_plant_ok.index)
-
-# from df_plant_wrong remove products that exist in df_plant_ok
-df_plant_wrong = df_plant_wrong.drop(
-    df_plant_wrong[df_plant_wrong['Material'].isin(df_plant_ok['Material'].unique())].index)
-
-# from df_plant_wrong remove products without price
-df_plant_wrong = df_plant_wrong[~df_plant_wrong['Amount_(Currency)'].isna()]
-
-# print(df_plant_wrong)
-
-
-df_output = pd.concat([df_plant_wrong, df_plant_ok])
-# print(df_output)
-
-df_output = df_output[df_output[['Amount_(Currency)', 'B600_accepted', 'Plant_accepted']].any(axis=1)]
+df_merge_pipeline()
+autoreplacement_evaluation()
 
 if file_exists(out_file['name']):
     print(f'Replacing {out_file["name"]}\n')
@@ -186,6 +212,8 @@ else:
     print(f'Creating {out_file["name"]}\n')
 
 with pd.ExcelWriter(out_file["name"], mode='w') as writer:
-    df_output.to_excel(writer, sheet_name='Total', index=False)
+    df_output.to_excel(writer, sheet_name=out_file["status_sheet"], index=False)
+    df_auroreplacement.to_excel(
+        writer, sheet_name=out_file["autoreplacement"], index=False)
 
 input('Done. Press any key to exit...')
